ed04fd40eb5c92873ea77380453b269ddfac0b25
qpzm <lhm1442@gmail.com>
2019-01-24 15:04:37 +0900

    1.1.7 Use lexical scoping to omit repeating x

--- 1.1/sqrt.rkt
+++ 1.1/sqrt.rkt
@@ -1,29 +1,25 @@
 #lang sicp
 
 (define (sqrt x)
-  (sqrt-iter 1.0 x)
-)
-
-(define (sqrt-iter guess x)
-    (if (good-enough? guess x)
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
       guess
-      (sqrt-iter (improve guess x) x)))
+      (sqrt-iter (improve guess))))
 
-(define (improve guess x)
-  (average guess (/ x guess))
-)
+  (define (improve guess)
+    (average guess (/ x guess)))
 
-(define (average x y)
-  (/ (+ x y) 2)
-)
+  (define (average x y)
+    (/ (+ x y) 2))
 
-(define (good-enough? guess x)
+  (define (good-enough? guess)
     (> 0.001
-       (abs (- (square guess) x)))
-)
+       (abs (- (square guess) x))))
 
-(define (square x)
-    (* x x)
+  (define (square x)
+    (* x x))
+
+  (sqrt-iter 1.0)
 )
 
 (write (number->string (sqrt 4)))
